b0a6cd6707900989b0201947cb45e9a797a837fb
KC Lim <kclim0528@gmail.com>
2024-05-24 00:01:28 -0700

    removed api key

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const weatherForm = document.querySelector(".weatherForm");
 const cityInput = document.querySelector(".cityInput");
 const card = document.querySelector(".card");
-const apiKey = "1f97812daeff8c96a42a0be082fb5398";
+const apiKey = //retrieve from postman appid;
 
 weatherForm.addEventListener("submit", async (event) => {
   event.preventDefault();
